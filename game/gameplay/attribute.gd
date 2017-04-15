4f7dfe432303ade36ac2ddc5b0626e19891a5abc
Khin Baptista <khin.baptista@gmail.com>
2017-04-14 23:32:41 -0300

    Added movement

--- game/gameplay/attribute.gd
+++ game/gameplay/attribute.gd
@@ -1,23 +1,19 @@
 extends Node
 
-################################################################################
-
-export var value = 0.0 setget set_value
-export var max_value = 100.0 setget set_max_base
-export var multiplier = 1.0 setget set_multiplier
-export var addition = 0.0 setget set_addition
+export var value	= 0.0		setget set_value
+export var max_value	= 5000.0	setget set_max_base
+export var multiplier	= 1.0		setget set_multiplier
+export var addition	= 0.0		setget set_addition
 
 signal changed		# Emitted when the current value is changed
 signal changed_max	# Emitted when the max value is changed
 
-################################################################################
-
 func _ready():
-	_clamp_value()
+	clamp_value()
+	print(get_name(), "\t", value, " / ", get_max())
 
-#	Clamp current value between 0 and modified max value, emit signal optionally
-func _clamp_value(signalize = false):
-	var maxval = get_max_value()
+func clamp_value(signalize = false):
+	var maxval = get_max()
 	var changed = false
 	
 	if value > maxval:
@@ -30,23 +26,17 @@ func _clamp_value(signalize = false):
 	if signalize and changed:
 		emit_signal("changed")
 
-################################################################################
-
-func get_max_value():
+func get_max():
 	return max_value * multiplier + addition
-
-##########	Modify current value
 
 func set_value(val):
 	value = val
-	_clamp_value()
+	clamp_value()
 	
 	emit_signal("changed")
 
 func add_value(val):
 	set_value(value + val)
-
-##########	Modify maximum value
 
 func set_max_base(val):
 	max_value = val
@@ -60,11 +50,7 @@ func set_multiplier(factor):
 	multiplier = factor
 	emit_signal("changed_max")
 
-##########
-
 func clear():
 	addition = 0.0
 	multiplier = 1.0
 	emit_signal("changed_max")
-
-################################################################################
--- game/player/movement.gd
+++ game/player/movement.gd
@@ -6,3 +6,19 @@ var movement_attr
 func _ready():
 	player = get_parent()
 	movement_attr = player.get_node("attributes/movement_speed")
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	var movement = Vector2()
+	
+	if Input.is_action_pressed("move_up"):
+		movement.y -= 1.0
+	if Input.is_action_pressed("move_down"):
+		movement.y += 1.0
+	if Input.is_action_pressed("move_left"):
+		movement.x -= 1.0
+	if Input.is_action_pressed("move_right"):
+		movement.x += 1.0
+	
+	movement = movement.normalized()
+	player.move(movement * movement_attr.value * delta)
