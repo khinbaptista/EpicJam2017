0069655d879ffd664cc683d8972cdd9213dc859a
Khin Baptista <khin.baptista@gmail.com>
2017-04-16 15:08:37 -0300

    Attempted to fix hole

--- game/dungeon/Hole.gd
+++ game/dungeon/Hole.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
 func _ready():
-	connect("body_enter", self, "on_player_enter")
+	connect("body_enter", self, "on_body_enter")
 
-func on_player_enter(body):
+func on_body_enter(body):
 	if(body.get_name() == "player"):
 		body.kill()
