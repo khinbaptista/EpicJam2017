6859c4e062d73e03bab367030bd2b21a953e6548
Khin Baptista <khin.baptista@gmail.com>
2017-04-16 02:15:03 -0300

    Added player method 'kill'

--- game/player/player.gd
+++ game/player/player.gd
@@ -7,6 +7,9 @@ export(float) var pushback = 20.0
 func _ready():
 	get_tree().get_root().get_node("/root/death_listener").player = self
 
+func kill():
+	hit(get_node("attributes/health").value)
+
 func hit(damage, dir = Vector2()):
 	var health = get_node("attributes/health")
 	health.value -= damage
