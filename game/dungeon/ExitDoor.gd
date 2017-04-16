9ebc2b268e941884f70357a33841e1ac7964fc00
Khin Baptista <khin.baptista@gmail.com>
2017-04-16 03:15:55 -0300

    Added key logic to puzzle dungeon

--- game/dungeon/ExitDoor.gd
+++ game/dungeon/ExitDoor.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
+export(bool) var is_open = true
+
 func _ready():
 	connect("body_enter", self, "on_player_enter")
 
 func on_player_enter(body):
-	if(body.get_name() == "player"):
+	if(is_open and body.get_name() == "player"):
 		get_node("/root/ScenesData").loadNextScene()
--- game/dungeon/Key.gd
+++ game/dungeon/Key.gd
@@ -1,12 +1,9 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
 	connect("body_enter", self, "on_player_enter")
 
 func on_player_enter(body):
 	if(body.get_name() == "player"):
-		hide()
+		get_node("../ExitDoor").is_open = true
+		queue_free()
